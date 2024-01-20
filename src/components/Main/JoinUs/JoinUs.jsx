de7525d1fef4da9bb89e497b26143130c34dacf7
kenkindermann <ken.kindermann@gmail.com>
2024-01-20 18:06:58 +0100

    added big header, subtitle and discord button

--- src/components/Main/JoinUs/JoinUs.jsx
+++ src/components/Main/JoinUs/JoinUs.jsx
@@ -3,6 +3,7 @@ const JoinUs = () => {
     <section id="join-us">
       <h2 className="big-header">Join us via Discord</h2>
       <h3 className="subtitle">be part of our crew</h3>
+      <button>discord</button>
     </section>
   );
 };
