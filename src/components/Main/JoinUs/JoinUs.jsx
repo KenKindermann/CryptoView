abe8ef6ecf522c2a52d78f49d445f69375806bf7
kenkindermann <ken.kindermann@gmail.com>
2024-01-20 18:11:54 +0100

    changed button to a tag, set text-decoration to none in boilerplate

--- src/components/Main/JoinUs/JoinUs.jsx
+++ src/components/Main/JoinUs/JoinUs.jsx
@@ -3,7 +3,9 @@ const JoinUs = () => {
     <section id="join-us">
       <h2 className="big-header">Join us via Discord</h2>
       <h3 className="subtitle">be part of our crew</h3>
-      <button>discord</button>
+      <a href="https://discord.com" target="_blank">
+        discord
+      </a>
     </section>
   );
 };
