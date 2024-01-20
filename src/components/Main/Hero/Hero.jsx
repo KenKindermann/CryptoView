a21559d611e83c4cb612f6c7356665bbca6ccb7a
kenkindermann <ken.kindermann@gmail.com>
2024-01-20 18:03:23 +0100

    created joinUs component, added classes for big header and subtitle

--- src/components/Main/Hero/Hero.jsx
+++ src/components/Main/Hero/Hero.jsx
@@ -3,8 +3,8 @@ import arrowDown from "/src/assets/icons/arrow_downward_FILL0_wght400_GRAD0_opsz
 const Hero = () => {
   return (
     <section id="hero">
-      <h2>Track and Trade</h2>
-      <h1>Crypto Currencies</h1>
+      <h2 className="subtitle">Track and Trade</h2>
+      <h1 className="big-header">Crypto Currencies</h1>
       <img src={arrowDown} alt="arrow downward" />
     </section>
   );
---
+++ src/components/Main/JoinUs/JoinUs.jsx
@@ -0,0 +1,10 @@
+const JoinUs = () => {
+  return (
+    <section id="join-us">
+      <h2 className="big-header">Join us via Discord</h2>
+      <h3 className="subtitle">be part of our crew</h3>
+    </section>
+  );
+};
+
+export default JoinUs;
