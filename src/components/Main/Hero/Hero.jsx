a5b23a5473fa74984c573a939bc3ea3bdee9a7b7
kenkindermann <ken.kindermann@gmail.com>
2024-01-20 14:44:43 +0100

    change headers sizes, add margin 0 to css reset

--- src/components/Main/Hero/Hero.jsx
+++ src/components/Main/Hero/Hero.jsx
@@ -1,5 +1,10 @@
 const Hero = () => {
-  return <div></div>;
+  return (
+    <section id="hero">
+      <h2>Track and Trade</h2>
+      <h1>Crypto Currencies</h1>
+    </section>
+  );
 };
 
 export default Hero;
