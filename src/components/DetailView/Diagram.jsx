e668d40452070b9e582308d0c5c65bdb3b406406
kenkindermann <ken.kindermann@gmail.com>
2024-01-27 12:59:23 +0100

    diagram data changes by changing period

--- src/components/DetailView/Diagram.jsx
+++ src/components/DetailView/Diagram.jsx
@@ -1,6 +1,6 @@
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts";
 import useFetch from "../../hooks/useFetch";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import moment from "moment";
 import test from "./testObject.js";
 
@@ -8,10 +8,27 @@ const Diagram = ({ id }) => {
   const { fetchData } = useFetch();
   const [history, setHistory] = useState(null);
   const [formattedData, setFormattedData] = useState();
-  const [uniqueMonths, setUniqueMonths] = useState();
+  const [uniqueTicks, setUniqueTicks] = useState();
   const [activePeriod, setActivePeriod] = useState("12 month");
 
-  const periods = ["1 month", "3 month", "6 month", "12 month"];
+  const periods = [
+    {
+      label: "1 month",
+      substract: 1,
+    },
+    {
+      label: "3 month",
+      substract: 3,
+    },
+    {
+      label: "6 month",
+      substract: 6,
+    },
+    {
+      label: "12 month",
+      substract: 12,
+    },
+  ];
 
   // useEffect(() => {
   //   if (id) {
@@ -25,8 +42,14 @@ const Diagram = ({ id }) => {
   }, []);
 
   useEffect(() => {
+    if (formattedData) {
+      setTicks("12 month");
+    }
+  }, [formattedData]);
+
+  // Formatting history data from api to needed structure for diagram
+  useEffect(() => {
     if (history) {
-      console.log("HISTORY", history);
       const formattedData = history.map((item) => ({
         month: moment(item[0]).format("MMM YY"),
         time: moment(item[0]).format("YYYY-MM-DD"),
@@ -37,50 +60,86 @@ const Diagram = ({ id }) => {
     }
   }, [history]);
 
-  useEffect(() => {
-    if (formattedData) {
-      const uniqueMonths = Array.from(
-        new Set(formattedData.map(({ time }) => moment(time).startOf("month").format("YYYY-MM-DD")))
-      );
-      setUniqueMonths(uniqueMonths);
-    }
-  }, [formattedData]);
-
+  // Format
   const formatXAxis = (tickItem) => {
     const date = moment(tickItem, "YYYY-MM-DD");
-    return date.format("MMM YY");
+    switch (activePeriod) {
+      case "12 month":
+        return date.format("MMM YY");
+      case "6 month":
+        return date.format("MMM YY");
+      case "3 month":
+        return date.format("WW");
+      case "1 month":
+        return date.format("DD.MM.");
+      default:
+        return date.format("MMM YY");
+    }
   };
 
-  useEffect(() => {
-    console.log(activePeriod);
-  }, [activePeriod]);
+  // Format ticks by current period
+  const setTicks = (currentPeriod) => {
+    let ticks;
+    switch (currentPeriod) {
+      case "3 month":
+        ticks = Array.from(
+          new Set(formattedData?.map(({ time }) => moment(time).startOf("week").format("YYYY-MM-DD")))
+        );
+        break;
+      case "1 month":
+        ticks = formattedData?.map(({ time }) => moment(time).format("YYYY-MM-DD"));
+        break;
+      default:
+        ticks = Array.from(
+          new Set(formattedData?.map(({ time }) => moment(time).startOf("month").format("YYYY-MM-DD")))
+        );
+        break;
+    }
+    setUniqueTicks(ticks);
+    setActivePeriod(currentPeriod);
+  };
+
+  // Filter data by current period
+  const showPeriodData = () => {
+    const now = moment();
+    const currentPeriod = periods.find((period) => period.label === activePeriod);
+    return formattedData.filter((data) =>
+      moment(data.time).isAfter(now.clone().subtract(currentPeriod.substract, "months"))
+    );
+  };
 
   return (
-    <>
-      <div className="diagram">
-        <LineChart width={800} height={300} data={formattedData}>
-          <Line type="monotone" dataKey="price" stroke="#002c45" dot={false} />
-          <CartesianGrid stroke="#ccc" />
-          <XAxis
-            dataKey="time"
-            ticks={uniqueMonths}
-            tickFormatter={formatXAxis}
-            interval={0}
-            tick={{ fontSize: 10, angle: -45, dy: 10 }}
-          />
-          <YAxis />
-          <Tooltip labelFormatter={(value) => moment(value, "YYYY-MM-DD").format("DD.MM.YYYY")} />
-        </LineChart>
-      </div>
-      <div className="choose-period">
-        {periods.map((period) => (
-          <button className={activePeriod === period ? `active` : null} onClick={() => setActivePeriod(period)}>
-            {period}
-          </button>
-        ))}
-      </div>
-    </>
+    formattedData && (
+      <>
+        <div className="diagram">
+          <LineChart key={uniqueTicks} width={900} height={300} data={showPeriodData()}>
+            <Line type="monotone" dataKey="price" stroke="#002c45" dot={false} />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis
+              dataKey="time"
+              ticks={uniqueTicks}
+              tickFormatter={formatXAxis}
+              interval={0}
+              tick={{ fontSize: 10, angle: -45, dy: 10 }}
+            />
+            <YAxis />
+            <Tooltip labelFormatter={(value) => moment(value, "YYYY-MM-DD").format("DD.MM.YYYY")} />
+          </LineChart>
+        </div>
+
+        <div className="choose-period">
+          {periods.map((period) => (
+            <button
+              key={period.label}
+              className={activePeriod === period.label ? `active` : null}
+              onClick={() => setTicks(period.label)}
+            >
+              {period.label}
+            </button>
+          ))}
+        </div>
+      </>
+    )
   );
 };
-
 export default Diagram;
