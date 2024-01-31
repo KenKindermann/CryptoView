2de4061d6edad41f9fa77a769bb087696a02d538
kenkindermann <ken.kindermann@gmail.com>
2024-01-31 16:54:07 +0100

    diagram is responsive

--- src/components/DetailView/Diagram.jsx
+++ src/components/DetailView/Diagram.jsx
@@ -1,4 +1,4 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 import useFetch from "../../hooks/useFetch";
 import { useEffect, useState } from "react";
 import moment from "moment";
@@ -39,20 +39,24 @@ const Diagram = ({ id }) => {
   return (
     history && (
       <>
-        <div className="diagram">
-          <LineChart key={uniqueTicks} width={900} height={300} data={showPeriodData()}>
-            <Line type="monotone" dataKey="price" stroke="#002c45" dot={false} />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis
-              dataKey="time"
-              ticks={uniqueTicks}
-              tickFormatter={formatXAxis}
-              interval={0}
-              tick={{ fontSize: 10, angle: -45, dy: 10 }}
-            />
-            <YAxis />
-            <Tooltip labelFormatter={(value) => moment(value, "YYYY-MM-DD").format("DD.MM.YYYY")} />
-          </LineChart>
+        <div className="diagram-container">
+          <div className="diagram">
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart key={uniqueTicks} data={showPeriodData()}>
+                <Line type="monotone" dataKey="price" stroke="#002c45" dot={false} />
+                <CartesianGrid stroke="#ccc" />
+                <XAxis
+                  dataKey="time"
+                  ticks={uniqueTicks}
+                  tickFormatter={formatXAxis}
+                  interval={0}
+                  tick={{ fontSize: 10, angle: -45, dy: 10 }}
+                />
+                <YAxis />
+                <Tooltip labelFormatter={(value) => moment(value, "YYYY-MM-DD").format("DD.MM.YYYY")} />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
         </div>
 
         <div className="choose-period">
