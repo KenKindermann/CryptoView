56b0b53aae005914987cb08787433c709156f60a
kenkindermann <ken.kindermann@gmail.com>
2024-02-01 10:48:00 +0100

    added comments to diagram.jsx

--- src/components/DetailView/Diagram.jsx
+++ src/components/DetailView/Diagram.jsx
@@ -10,6 +10,7 @@ const Diagram = ({ id }) => {
   const [activePeriod, setActivePeriod] = useState(periods[3]);
   const [uniqueTicks, setUniqueTicks] = useState();
 
+  // Fetch data using useFetch hook when id is changing
   useEffect(() => {
     if (id) {
       const url = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=365&interval=daily&precision=0`;
@@ -17,13 +18,14 @@ const Diagram = ({ id }) => {
     }
   }, [id]);
 
+  // Set unique ticks by getTicks object method when history is changing
   useEffect(() => {
     if (history) {
       setUniqueTicks(activePeriod.getTicks(history));
     }
   }, [history]);
 
-  // Format
+  // Format x axis
   const formatXAxis = (tickItem) => {
     const date = moment(tickItem, "YYYY-MM-DD");
     return date.format(activePeriod.dateFormat);
