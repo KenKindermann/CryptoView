1398e56037d3d25547d9b7017fc243f738753c1a
kenkindermann <ken.kindermann@gmail.com>
2024-02-01 09:34:48 +0100

    corrected typo in coingecko link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer>
       <p>Copyright 2024.</p>
       <span>Data from</span>
-      <a href="https://goingecko.com" target="_blank">
+      <a href="https://coingecko.com" target="_blank">
         <img src="/src/assets/images/coingecko_logo_with_text_biw_logo_with_dark_text copy.svg" alt="coin gecko logo" />
       </a>
     </footer>
